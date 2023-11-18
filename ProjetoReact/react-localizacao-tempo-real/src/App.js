a5bf9538bed82ea8fcb9c0fceb6dd69e7ba30a35
Diogotb <digogo@gmail.com>
2023-11-17 21:52:47 -0300

    17/11

---
+++ ProjetoReact/react-localizacao-tempo-real/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+import MapaAtual from './Mapa/MapaAtual';
+import MapaLongitudeLatitude from './Mapa/MapaLatitudeLongitude';
+import MapaCidade from './Mapa/MapaCidade';
+// Define o componente principal da aplicação, chamado 'App'.
+function App() {
+  return (
+
+    <div className="App">
+      <MapaCidade/>
+    </div>
+  );
+}
+// Exporta o componente 'App' como o componente principal da aplicação.
+export default App;
